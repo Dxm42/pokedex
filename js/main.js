408cc6b7fbf95528415b48d42f849354cb3aa41c
lucas <lsantana1234@hotmail.com>
2023-03-27 00:12:29 -0300

    :books: Atualizado o README

--- js/main.js
+++ js/main.js
@@ -1,31 +1,41 @@
+const pokemonList  = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 10
+let offset = 0
 
-function convertpokemonToHtml(pokemon){
+function convertPokemonToHtml(pokemon){
     return `
-    <li class="pokemon">
-    <span class="number">#001</span>
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">${pokemon.number}</span>
     <span class="name">${pokemon.name}</span>
 
     <div class="detail">
         <ol class="types">
-            <li class="type">grass</li>
-            <li class="type">poison</li>
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
 
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        <img src="${pokemon.photo}" alt="${pokemon.name}">
     </div>
 </li>  
     `
 }
+function loadPokemonItems(offset, limit){
+   
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        
+        const newList = pokemons.map((pokemon) => {
+            return convertPokemonToHtml(pokemon)
+    
+        })
+        const newHtml = newList.join('')
+    
+        pokemonList.innerHTML += newHtml           
+        
+    })
+}
+loadPokemonItems(offset, limit)
 
-pokeApi.getPokemons().then((pokemons) => {
-    const listItems = []
-    for(let i = 0; i < pokemons.length; i++){
-      const pokemon = pokemons[i];
-      listItems.push(convertpokemonToHtml(pokemon))
-    }
-    console.log(listItems)
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItems(offset, limit)
 })
-.catch((error) => {
-    console.log(error)
-})
-
